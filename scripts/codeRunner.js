0db17e382c1b66a295192b607ff81e991c22bd78
Haoran Yang <54178753+Lazenander@users.noreply.github.com>
2022-09-18 11:05:46 +0800

    update

--- scripts/codeRunner.js
+++ scripts/codeRunner.js
@@ -1,11 +1,56 @@
 import DataStream from "./DataStream.js";
 import VariableTable from "./VariableTable.js";
 
+function calculateDataBlock(index) {
+    console.log("Calculating " + index);
+    let innerDataStream = [];
+    let inputDataStream = [];
+    if (blocks[index].type == "input") {
+        let ds = new DataStream();
+        ds.read(inputs[index][0]);
+        console.log(ds);
+        innerDataStream.push(ds);
+    } else {
+        for (let i = 0; i < blocks[index].dataImports.length; i++) {
+            if (blocks[index].dataImports[i] == -1) {
+                inputDataStream.push(new DataStream());
+                continue;
+            }
+            inputDataStream.push(calculateDataBlock(blocks[index].dataImports[i]).dataOutput[0]);
+        }
+    }
+    return blocks[index].forward(innerDataStream, inputDataStream);
+}
+
+let constIndegree = {};
+let constRegionCnt = {};
+let regionTable = {},
+    region2Table = {};
+let regionTree = {};
+
+function initialize(q) {
+    let calIndegree = {...constIndegree };
+
+    while (q.length) {
+        let currentIndex = q.shift();
+
+        for (let i = 0; i < blocks[currentIndex].logicExports.length; i++) {
+            for (let j = 0; j < blocks[currentIndex].logicImports[i].length; j++) {
+                calIndegree[blocks[currentIndex].logicExports[i][j]]--;
+
+                if (calIndegree[blocks[currentIndex].logicExports[i][j]] == 0)
+                    q.push(blocks[currentIndex].logicExports[i][j]);
+            }
+        }
+    }
+}
+
+function forwardGraph(q) {}
+
 self.onmessage = (e) => {
     let blocks = JSON.parse(e.data).blocks;
     let inputs = JSON.parse(e.data).inputs;
 
-    let constIndegree = {};
     let q = [];
 
     for (let i in blocks) {
@@ -19,33 +64,10 @@ self.onmessage = (e) => {
 
     let calIndegree = {...constIndegree };
 
-    function calculateDataBlock(index) {
-        console.log("Calculating " + index);
-        let innerDataStream = [];
-        let inputDataStream = [];
-        if (blocks[index].type == "input") {
-            let ds = new DataStream();
-            ds.read(inputs[index][0]);
-            console.log(ds);
-            innerDataStream.push(ds);
-        } else {
-            for (let i = 0; i < blocks[index].dataImports.length; i++) {
-                if (blocks[index].dataImports[i] == -1) {
-                    inputDataStream.push(new DataStream());
-                    continue;
-                }
-                inputDataStream.push(calculateDataBlock(blocks[index].dataImports[i]).dataOutput[0]);
-            }
-        }
-        return blocks[index].forward(innerDataStream, inputDataStream);
-    }
-    let constRegionCnt = {};
-    let regionTable = {};
-    let regionTree = {};
-
     constRegionCnt[-1] = 0;
 
     for (let i in blocks) {
+        console.log(i, blocks);
         if (constIndegree[i] == 0) {
             q.push(i);
             regionTable[i] = -1;
@@ -55,32 +77,43 @@ self.onmessage = (e) => {
 
     while (q.length > 0) {
         let currentIndex = q.shift();
+        console.log(currentIndex);
         for (let i = 0; i < blocks[currentIndex].logicExports.length; i++) {
-            calIndegree[blocks[currentIndex].logicExports[i]] -= 1;
+            for (let j = 0; j < blocks[currentIndex].logicImports[i].length; j++) {
+                calIndegree[blocks[currentIndex].logicExports[i][j]] -= 1;
 
-            if (calIndegree[blocks[currentIndex].logicExports[i]] == 0) {
-                q.push(blocks[currentIndex].logicExports[i]);
+                if (calIndegree[blocks[currentIndex].logicExports[i][j]] == 0) {
+                    q.push(blocks[currentIndex].logicExports[i][j]);
 
-                if (blocks[currentIndex].type == "switch" && i != 2) {
-                    regionTable[blocks[currentIndex].logicExports[i]] = currentIndex + "_" + i;
-                    if (!(regionTable[currentIndex] in regionTree))
-                        regionTree[regionTable[currentIndex]] = [];
-                    regionTree[regionTable[currentIndex]].push(currentIndex + "_" + i);
-                } else if (blocks[currentIndex].type == "loop" && i != 1) {
-                    regionTable[blocks[currentIndex].logicExports[i]] = currentIndex + "";
-                    if (!(regionTable[currentIndex] in regionTree))
-                        regionTree[regionTable[currentIndex]] = [];
-                    regionTree[regionTable[currentIndex]].push(currentIndex + "");
-                } else
-                    regionTable[blocks[currentIndex].logicExports[i]] = regionTable[currentIndex];
+                    if (blocks[currentIndex].type == "switch" && i != 2) {
+                        regionTable[blocks[currentIndex].logicExports[i][j]] = currentIndex + "_" + i;
+                        if (!(currentIndex in region2Table))
+                            region2Table[currentIndex] = [];
+                        region2Table[currentIndex].push(blocks[currentIndex].logicExports[i][j]);
+                        if (!(regionTable[currentIndex] in regionTree))
+                            regionTree[regionTable[currentIndex]] = [];
+                        regionTree[regionTable[currentIndex]].push(currentIndex + "_" + i);
+                    } else if (blocks[currentIndex].type == "loop" && i != 1) {
+                        regionTable[blocks[currentIndex].logicExports[i][j]] = currentIndex + "";
+                        if (!(currentIndex in region2Table))
+                            region2Table[currentIndex] = [];
+                        region2Table[currentIndex].push(blocks[currentIndex].logicExports[i][j]);
+                        if (!(regionTable[currentIndex] in regionTree))
+                            regionTree[regionTable[currentIndex]] = [];
+                        regionTree[regionTable[currentIndex]].push(currentIndex + "");
+                    } else
+                        regionTable[blocks[currentIndex].logicExports[i][j]] = regionTable[currentIndex];
 
-                if (regionTable[blocks[currentIndex].logicExports[i]] in constRegionCnt)
-                    constRegionCnt[regionTable[blocks[currentIndex].logicExports[i]]]++;
-                else
-                    constRegionCnt[regionTable[blocks[currentIndex].logicExports[i]]] = 1;
+                    if (regionTable[blocks[currentIndex].logicExports[i][j]] in constRegionCnt)
+                        constRegionCnt[regionTable[blocks[currentIndex].logicExports[i][j]]]++;
+                    else
+                        constRegionCnt[regionTable[blocks[currentIndex].logicExports[i][j]]] = 1;
+                }
             }
         }
     }
+
+    console.log(constRegionCnt, regionTable, region2Table, regionTree);
 
     calIndegree = {...constIndegree };
     let calRegionCnt = {...constRegionCnt };
@@ -93,16 +126,20 @@ self.onmessage = (e) => {
         let currentIndex = q.shift();
         let innerDataStream = [],
             inputDataStream = [];
+        let currentRegion = parseInt(regionTable[currentIndex])
         if (blocks[currentIndex] != "loop")
-            calRegionCnt[regionTable[currentIndex]]--;
+            calRegionCnt[currentRegion]--;
         if (blocks[currentIndex] == "assign") {
             let ds = new DataStream();
             ds.read(inputs[index]);
             innerDataStream.push(ds);
         }
         for (let i = 0; i < blocks[currentIndex].dataImports.length; i++)
-            inputDataStream.push(calculateDataBlock(blocks[currentIndex].dataImports[i]).dataOutput[0]);
-        blocks[currentIndex].forward(innerDataStream, inputDataStream);
+            if (blocks[currentIndex].dataImports[i] != -1)
+                inputDataStream.push(calculateDataBlock(blocks[currentIndex].dataImports[i]).dataOutput[0]);
+        let res = blocks[currentIndex].forward(innerDataStream, inputDataStream);
+        if (blocks[currentIndex] == "loop" && res.logicport == blocks[currentIndex].logicExports.length - 1)
+            calRegionCnt[currentRegion]--;
         console.log(inputDataStream[0].data);
         if (blocks[currentIndex].type == "output") {
             if (inputDataStream[0].type == "string")
@@ -112,18 +149,27 @@ self.onmessage = (e) => {
             else
                 postMessage({ type: "output", data: { index: currentIndex, context: "" + inputDataStream[0].data } });
         }
-        /*
-                if (calRegionCnt[regionTable[currentIndex]] == 0) {
-                    if (blocks[regionTable[currentIndex]].type == "switch")
-                        for (let i = 0; i < blocks[regionTable[currentIndex]].logicExports[blocks[regionTable[currentIndex]].logicExports.length - 1].length; i++)
-                            q.push(blocks[regionTable[currentIndex]].logicExports[blocks[regionTable[currentIndex]].logicExports.length - 1][i]);
-                    else if (blocks[regionTable[currentIndex]].type == "loop") {
-                        q.push(regionTable[currentIndex]);
-                    }
-                }*/
-    }
 
-    console.log(calRegionCnt, regionTable);
+        for (let i = 0; i < blocks[currentIndex].logicExports[res.logicport].length; i++)
+            q.push(blocks[currentIndex].logicExports[res.logicport][0]);
+
+        if (currentRegion != -1 && calRegionCnt[currentRegion] == 0) {
+            if (blocks[currentRegion].type == "switch")
+                for (let i = 0; i < blocks[currentRegion].logicExports[blocks[currentRegion].logicExports.length - 1].length; i++)
+                    q.push(blocks[currentRegion].logicExports[blocks[currentRegion].logicExports.length - 1][i]);
+            else if (blocks[currentRegion].type == "loop") {
+                function clearRegion(index) {
+                    calRegionCnt[index] = constRegionCnt[index];
+                    for (let i = 0; i < region2Table[index].length; i++)
+                        calIndegree[region2Table[index][i]] = constIndegree[region2Table[index][i]];
+                    for (let i in regionTree[index])
+                        clearRegion(regionTree[index][i]);
+                }
+                clearRegion(currentRegion);
+                q.push(currentRegion);
+            }
+        }
+    }
 
     postMessage({ type: "signal", data: "End" });
 }
